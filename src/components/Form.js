88dc29da1ca35e66baa1c8a974ffa80ace9e3237
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-27 18:42:17 +0100

    ✨feat(react): liaison de l'API movie et création des cards

---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import MyCard from './MyCard';
+
+const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=89360935952806bb62e1575a82c32b69&query=${search}&language=fr-FR').then((res) => setMoviesData(res.data.results));
+  }, []);
+
+  return (
+    <div className="form-component">
+      <div className="form-container">
+        <form>
+          <input type="text" placeholder="Entre le titre d'un film" id="search-input" />
+          <input type="submit" value="Rechercher" />
+        </form>
+        <div className="btn-sort-container">
+            <div className="btn-sort" id="goodToBad">Top<span>→</span></div>
+            <div className="btn-sort" id="badToGood">Flop<span>→</span></div>
+        </div>
+      </div>
+      <div className="result">
+        {moviesData.slice(0,12)
+        .map((movie) => (
+          <MyCard key={movie.id} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Form;
