eab1326580fe0c78e79b044fb833f7d559252711
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-28 17:35:12 +0100

    ✨feat(react): ajout du synopsis

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Avatar } from 'antd';
+import { Card } from 'antd';
 import { HeartFilled } from '@ant-design/icons';
 const { Meta } = Card;
 
@@ -7,6 +7,74 @@ const MyCard = ({ movie }) => {
   const dateFormater = (date) => {
     let [yy, mm, dd] = date.split("-");
     return [dd, mm, yy].join('/');
+  };
+
+  const genreFinder = () => {
+    let genreArray = [];
+    for (let i = 0; i < movie.genre_ids.length; i++) {
+      switch (movie.genre_ids[i]) {
+        case 28:
+          genreArray.push(`Action`);
+          break;
+        case 12:
+          genreArray.push(`Aventure`);
+          break;
+        case 16:
+          genreArray.push(`Animation`);
+          break;
+        case 35:
+          genreArray.push(`Comédie`);
+          break;
+        case 80:
+          genreArray.push(`Policier`);
+          break;
+        case 99:
+          genreArray.push(`Documentaire`);
+          break;
+        case 18:
+          genreArray.push(`Drame`);
+          break;
+        case 10751:
+          genreArray.push(`Famille`);
+          break;
+        case 14:
+          genreArray.push(`Fantasy`);
+          break;
+        case 36:
+          genreArray.push(`Histoire`);
+          break;
+        case 27:
+          genreArray.push(`Horreur`);
+          break;
+        case 10402:
+          genreArray.push(`Musique`);
+          break;
+        case 9648:
+          genreArray.push(`Mystère`);
+          break;
+        case 10749:
+          genreArray.push(`Romance`);
+          break;
+        case 878:
+          genreArray.push(`Science-fiction`);
+          break;
+        case 10770:
+          genreArray.push(`Téléfilm`);
+          break;
+        case 53:
+          genreArray.push(`Thriller`);
+          break;
+        case 10752:
+          genreArray.push(`Guerre`);
+          break;
+        case 37:
+          genreArray.push(`Western`);
+          break;
+        default:
+          break;
+      }
+      return genreArray.map((genre) => <li key={genre}>{genre}</li>)
+    }
   };
 
   return (
@@ -31,11 +99,14 @@ const MyCard = ({ movie }) => {
               <h5>Sorti le : {dateFormater(movie.release_date)}</h5>
               ) : ""}
               <h4>{movie.vote_average}/10<span>⭐</span></h4>
+
+              <ul>{genreFinder()}</ul>
             </p>
             <Meta
               title={movie.title}
-              description="This is the description"
+              description={movie.overview ? <h3>Synopsis</h3> : ""}
             />
+            <p>{movie.overview}</p>
           </Card>
     </div>
   );
